5c78945b1c1dd95bae64974e7c7a5902f8c21eee
emmachancellor <ecdyer@sbcglobal.net>
2024-07-10 09:58:05 -0400

    Updating print statements in compare_tcm()

--- compare_tcm.py
+++ compare_tcm.py
@@ -1,5 +1,5 @@
 # Emma C. Dyer
-# Last Updated: 8 July 2024
+# Last Updated: 9 July 2024
 
 import numpy as np
 import pandas as pd
@@ -17,39 +17,35 @@ from smallestEnclosingCircle import make_circle
 from sklearn.mixture import GaussianMixture
 from scipy.stats import ks_2samp
 
-grid_file = '/michorlab/ecdyer/multiplex_spatial/crc_grid_data/CRC17_tile_ROIs.h5'
+# Note for self: Can save stdout and stderr to file by using the following when running the script:
+# python3 your_script.py > output.txt 2>&1
 
-markers = ['CD45_status', 'SMA_status']
+if __name__ == '__main__':
 
-keep_cols = ['X_centroid', 'Y_centroid', 'CellID']
+    cwd = '/michorlab/ecdyer/multiplex_spatial/crc_grid_data/cph_grids'
+    short_grid_files = os.listdir(cwd)
+    grid_files = [os.path.join(cwd, f) for f in short_grid_files]
+    markers = ['CD45_status', 'SMA_status']
+    keep_cols = ['X_centroid', 'Y_centroid', 'CellID']
+    labels  = {1: 'Lymphocytes',
+                2: 'SMA'}
+    rename_cols_dict = {'X_centroid': 'x', 
+                        'Y_centroid': 'y'}
+    save_tcm_plot_path = '/michorlab/ecdyer/multiplex_spatial/figures/tcm_plots/'
+    save_csr_plot_path = '/michorlab/ecdyer/multiplex_spatial/figures/csr_tcm_plots/'
+    save_ks_results_path = '/michorlab/ecdyer/multiplex_spatial/tcm_results/'
+    save_tcm_path = '/michorlab/ecdyer/multiplex_spatial/tcm_results/'
+    save_csr_tcm_path = '/michorlab/ecdyer/multiplex_spatial/tcm_results/'
 
-labels  = {1: 'Lymphocytes',
-            2: 'SMA'}
-
-rename_cols_dict = {'X_centroid': 'x', 
-                    'Y_centroid': 'y'}
-
-save_csr_tcm_path = '/michorlab/ecdyer/multiplex_spatial/crc_grid_data/test_csr_files/'
-
-save_tcm_plot_path = '/michorlab/ecdyer/multiplex_spatial/figures/tcm_plots/'
-
-save_csr_plot_path = '/michorlab/ecdyer/multiplex_spatial/figures/csr_tcm_plots/'
-
-save_ks_results_path = '/michorlab/ecdyer/multiplex_spatial/tcm_results/'
-
-save_tcm_path = '/michorlab/ecdyer/multiplex_spatial/tcm_results/'
-
-save_csr_tcm_path = '/michorlab/ecdyer/multiplex_spatial/tcm_results/'
-
-ks_test_results = compare_tcm(grid_file, 
-            markers,
-            keep_cols,
-            labels,
-            visualiseStages=False,
-            save_tcm_plot_path=save_tcm_plot_path,
-            save_csr_plot_path=save_csr_plot_path,
-            save_tcm_path=save_tcm_path,
-            save_csr_tcm_path=save_csr_tcm_path,
-            save_ks_results_path=save_ks_results_path,
-            rename_cols_dict=rename_cols_dict,
-            plot_point_cloud=True)
+    ks_test_results = multithread_compare_tcm(grid_files, 
+                markers,
+                keep_cols,
+                labels,
+                visualiseStages=False,
+                #save_tcm_plot_path=save_tcm_plot_path,
+                #save_csr_plot_path=save_csr_plot_path,
+                #save_tcm_path=save_tcm_path,
+                #save_csr_tcm_path=save_csr_tcm_path,
+                save_ks_results_path=save_ks_results_path,
+                rename_cols_dict=rename_cols_dict,
+                plot_point_cloud=False)
--- tcm.sh
+++ tcm.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 #SBATCH --job-name=compare_tcm_job
-#SBATCH --output=compare_tcm_output_%j.txt
-#SBATCH --error=compare_tcm_error_%j.txt
+#SBATCH --output=compare_tcm_output.txt
+#SBATCH --error=compare_tcm_error.txt
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 #SBATCH --mem=32G
 #SBATCH --time=24:00:00
 #SBATCH --partition=defq
-#SBATCH --nodelist=node09
 
 # Load necessary modules
-module load python/3.8
+module load python3
 
 # Activate your Python environment if needed
-source /path/to/your/venv/bin/activate
+source ~/miniconda3/bin/activate scimap
 
 # Run the compare_tcm.py script
 srun python compare_tcm.py
