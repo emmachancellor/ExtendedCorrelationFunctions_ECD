22e09f83f20c5ad50f08ddf5bd517d294ef52c13
emmachancellor <ecdyer@sbcglobal.net>
2024-07-17 17:32:30 -0400

    complete generate_ks_grid_heatmap()

--- ecd_helperFunctions.py
+++ ecd_helperFunctions.py
@@ -8,6 +8,7 @@ import random
 import time
 import logging
 from functools import partial
+from matplotlib.cm import ScalarMappable
 from helperFunctions import *
 from smallestEnclosingCircle import make_circle
 from sklearn.mixture import GaussianMixture
@@ -740,22 +741,69 @@ def combine_clinical_and_tcm(clinical_data_file,
     clinical_data.to_csv(save_path, index=False)
     return
 
-def generate_ks_heatmap(grid_files,
+def get_sample_raw_files(data_dir,
+                         file_extension='.csv'):
+    """
+    Retrieves the file paths of all CSV files within the specified data directory and its subdirectories.
+
+    Args:
+        data_dir (str): The path to the data directory.
+        file_extension (str, optional): The file extension to search for. Defaults to '.csv'.
+    Returns:
+        list: A list of file paths for all files found with a specified file extension 
+            within the data directory and its subdirectories.
+    """
+    data_parent_dir = os.listdir(data_dir)
+    file_paths = []
+    for i in range(len(data_parent_dir)):
+        files = os.listdir(data_dir + data_parent_dir[i])
+        csv_files = [file for file in files if file.endswith(file_extension)]
+        for file in csv_files:
+            file_path = os.path.join(data_dir, data_parent_dir[i], file)
+            file_paths.append(file_path)
+    return file_paths
+
+
+def generate_ks_grid_heatmap(grid_files,
+                        sample_files,
                         results_directory,
                         grid_index_name='grid_location',
+                        ks_stat_col = 'ks_stat',
+                        x_coord = 'X_centroid',
+                        y_coord = 'Y_centroid',
                         display_plot=True,
                         save_plot=False,
                         bounding_box=True,
+                        save_directory=None,
+                        title_text = 'K-S Test', 
+                        cmap='viridis',
                         **kwargs):
     grid_dict = {}
     # Create a dictionary that holds the grid file paths and a sample label
-    for g in grid_files:
+    grid_files_list = os.listdir(grid_files)
+    grid_files_list = [os.path.join(grid_files, file) for file in grid_files_list]
+    for g in grid_files_list:
         sample_name = get_crc_sample_label(g)
-        grid_dict[sample_name] = grid_files + '/' + g
+        grid_dict[sample_name] = g
+    # Create a list of sample file paths
+    if os.path.isdir(sample_files) is True:
+        sample_files = get_sample_raw_files(sample_files)
+    elif os.path.isfile(sample_files) is True:
+        sample_files = [sample_files]
+    else: 
+        raise ValueError("Invalid sample_files path. Path must be a valid directory or file path.")
+    print("Sample Files: ", sample_files)
     for f in os.listdir(results_directory):
         if f.endswith('.csv'):
-            # Locate correct result file for the sample grid
+            # Locate correct result file and original sample file for the sample grid
             ks_sample_name = f[:5]
+            print('KS Sample Name: ', ks_sample_name)
+            sample_file = next((file for file in sample_files if ks_sample_name in file), None)
+            print('Sample File: ', sample_file)
+            if sample_file is not None:
+                sample_file_df = pd.read_csv(sample_file)
+            else:
+                raise ValueError(f"No sample file found for sample name: {sample_name}")
             ks_results = pd.read_csv(os.path.join(results_directory, f))
             ks_result_grid_file = grid_dict[ks_sample_name]
             ks_result_grid = load_tile_rois(ks_result_grid_file)
@@ -770,20 +818,44 @@ def generate_ks_heatmap(grid_files,
                     grid_data=ks_result_grid[grid_location]
                     if bounding_box is True:
                         # Calculate the bounding box
-                        min_x = float('inf')
-                        min_y = float('inf')
-                        max_x = float('-inf')
-                        max_y = float('-inf')
-                        # Iterate over each point in grid_location
-                        for point in grid_data['grid_location']:
-                            x, y = point
-                            min_x = min(min_x, x)
-                            min_y = min(min_y, y)
-                            max_x = max(max_x, x)
-                            max_y = max(max_y, y)
+                        min_x = grid_data['X_centroid'].min()
+                        max_x = grid_data['X_centroid'].max()
+                        min_y = grid_data['Y_centroid'].min()
+                        max_y = grid_data['Y_centroid'].max()
 
                         # Add the bounding box coordinates to ks_results
                         ks_results.at[index, 'min_x'] = min_x
                         ks_results.at[index, 'min_y'] = min_y
                         ks_results.at[index, 'max_x'] = max_x
                         ks_results.at[index, 'max_y'] = max_y
+            # Plot the heatmap for a given sample
+            x_min = ks_results['min_x']
+            x_max = ks_results['max_x']
+            y_min = ks_results['min_y']
+            y_max = ks_results['max_y']
+            ks_stat = ks_results[ks_stat_col]
+            # Simple baseline plot function
+            sns.set_style('white')
+            fig, ax = plt.subplots()
+            ax.plot(sample_file_df['X_centroid'], sample_file_df['Y_centroid'], 
+                    'o', color='lightgray', markersize=0.015, alpha=0.5, **kwargs)
+            
+                # Overlay translucent rectangles based on bounding box coordinates and normalized ks_stat values
+            for i in range(len(ks_results)):
+                rect = plt.Rectangle((x_min[i], y_min[i]), x_max[i] - x_min[i], y_max[i] - y_min[i],
+                                    color=sns.color_palette(cmap, as_cmap=True)(ks_stat[i]), alpha=0.3)
+                ax.add_patch(rect)
+            # Create a ScalarMappable and add the colorbar
+            sm = ScalarMappable(cmap=cmap)
+            sm.set_array([])
+            fig.colorbar(sm, ax=ax)
+            # Add plot title
+            ax.set_title(f'{ks_sample_name} {title_text} Heatmap')
+            if save_plot is True:
+                if save_directory is not None:
+                    plt.savefig(f'{save_directory}{ks_sample_name}_heatmap.png')
+                else:
+                    plt.savefig(f'{ks_sample_name}_heatmap.png', dpi=450)
+            if display_plot is True:
+                plt.show()
+    return
---
+++ result_heatmaps.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import pickle
+import random
+import time
+import logging
+import sys
+from functools import partial
+from joblib import Parallel, delayed
+from helperFunctions import *
+from ecd_helperFunctions import *
+from smallestEnclosingCircle import make_circle
+from sklearn.mixture import GaussianMixture
+from scipy.stats import ks_2samp
+
+if __name__ == '__main__':
+    grid_files = '/michorlab/ecdyer/multiplex_spatial/crc_grid_data/cph_grids/'
+    sample_files = '/michorlab/labsyspharm_ORION-CRC/labsyspharm_ORION-CRC/aws_data/'
+    results_directory = '/michorlab/ecdyer/multiplex_spatial/tcm_results/pdl1_cd45/'
+    save_directory = '/michorlab/ecdyer/multiplex_spatial/figures/pdl1_cd45_heatmaps/'
+
+    generate_ks_heatmap(grid_files,
+                         sample_files,
+                         results_directory,
+                         save_directory=save_directory,
+                         save_plot=True)
